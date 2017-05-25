84c5e648b1e051f298e5c89533cdcc1c11383b19
sojung kim <sojung kim>
2017-05-25 19:09:12 +0900

    #4 Event Programming

--- app/src/main/java/kr/hs/lia318e_mirim/contextmenutest/MainActivity.java
+++ app/src/main/java/kr/hs/lia318e_mirim/contextmenutest/MainActivity.java
@@ -1,22 +1,31 @@
 package kr.hs.lia318e_mirim.contextmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout Linear; // 전역 변수
+    Button but2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1 = (Button)findViewById(R.id.but1);
-        Button but2 = (Button)findViewById(R.id.but2);
+        but2 = (Button)findViewById(R.id.but2);
+
+        // 버튼을 길게 누르면 ContextMenu가 나온다.
         registerForContextMenu(but1);
         registerForContextMenu(but2);
+
+        Linear = (LinearLayout) findViewById(R.id.Linear);
     }
 
     @Override
@@ -33,5 +42,33 @@ public class MainActivity extends AppCompatActivity {
         if(v.getId() == R.id.but2){
             menuInflater.inflate(R.menu.menu2, menu);
         } // end of if
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+
+        switch (item.getItemId())
+        {
+            case R.id.menu1_item_red:
+                Linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu1_item_blue:
+                Linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_pink:
+                Linear.setBackgroundColor(Color.rgb(255,178,245)); // 메소드 사용 색 지정
+                return true;
+
+            case R.id.menu2_item_rotation:
+                but2.setRotation(90);
+                return true;
+            case R.id.menu2_item_size:
+                but2.setScaleX(1.5f); // 실수형 => f
+                but2.setScaleY(1.5f);
+                return true;
+        } // end of switch
+
+        return false;
     }
 }
